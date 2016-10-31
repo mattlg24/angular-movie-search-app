e598cc73390ab328789bfea35e5fdf4c020019fd
Matt Gardner <matthewlgardner@gmail.com>
2016-10-31 10:33:41 -0600

    added second controller for a single movie

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ app.config(function($routeProvider) {
             templateUrl: '../views/movies.html',
             controller: 'MoviesController'
         })
-        .when('/movie', {
+        .when('/movie/:id', {
             templateUrl: '../views/movie.html',
-            controller: 'MovieController'
+            controller: 'OneMovieController'
         })
 })
 
 
 app.controller('MoviesController', ['$scope', '$http', '$location', function($scope, $http, $location) {
-    $scope.thing = 'did this work?'
+    // $scope.thing = 'did this work?'
 
     // $scope.movie = {}
     $scope.movies = []
@@ -25,16 +25,26 @@ app.controller('MoviesController', ['$scope', '$http', '$location', function($sc
         console.log(movieTitle);
         $http.get(`http://www.omdbapi.com/?s=${movieTitle}`)
             .then(function(movies) {
-                console.log('movies data is', movies.data.Search);
+                console.log('movie data is', movies.data.Search);
                 // movies.push()
 
                 $scope.moviesData = movies.data.Search
                     // console.log(moviesData);
                 $location.url('/movies')
             })
-
     }
+}])
 
+app.controller('OneMovieController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+    let id = $routeParams.id
+    console.log('routeparams id is', id);
+    $http.get(`http://www.omdbapi.com/?i=${id}`)
+        .then(function(movie) {
+            console.log('movie is', movie);
+            $scope.movie = movie.data
+            console.log('movie  data', $scope.movie);
+        })
 
-
+    // $location.url('/movie/:id')
+    $scope.thing = 'did this work'
 }])
